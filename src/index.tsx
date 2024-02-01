1cae993fecccce080f3b252a68c3535e3d312c31
Teemu Silver <53870133+Tembero11@users.noreply.github.com>
2024-02-01 10:43:36 +0000

    Add simple routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import IndexPage from "./routes";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <IndexPage/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <p>Hello World</p>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
